c81672014e81fa8df02dfcfb4d86f12db08da839
Luis Fernando Gil Arroyo <luis.gil.arroyo@alumnos.upm.es>
2018-03-15 21:07:44 +0100

    fixed #1

--- testCSV.sh
+++ testCSV.sh
@@ -15,7 +15,12 @@ do
 	awk -v n=$numberfields 'BEGIN{FS=OFS=","} NF==n{count++} NF!=n{print "ERROR in file " FILENAME " line "   count+1 " Incorrect number of fields"; errors++; count++} END {if ( errors != 0  ) {exit 1}}' $file
 	if [[ $? -ne 0 ]]
 	then
-		exit 1
+		error="true"
 	fi
 	arguments=("${arguments[@]:2}")
 done
+
+if [ error == "true" ]
+then
+	exit 1
+fi
