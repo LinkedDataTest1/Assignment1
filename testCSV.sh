565a5e9915bf612d22712e6e03ffae1046944234
Luis Fernando Gil Arroyo <luis.gil.arroyo@alumnos.upm.es>
2018-03-15 20:07:25 +0100

    Update testCSV.sh

--- testCSV.sh
+++ testCSV.sh
@@ -12,7 +12,7 @@ while [[ ${#arguments[@]} > 0 ]]
 do
 	file=${arguments[0]}
 	numberfields=${arguments[1]}
-	awk -v n=$numberfields 'BEGIN{FS=OFS=","} NF==n{count++} NF!=n{print "ERROR in file " FILENAME " line "   count+1 " Incorrect number of fields\n" $0; exit 1}' $file
+	awk -v n=$numberfields 'BEGIN{FS=OFS=","} NF==n{count++} NF!=n{print "ERROR in file " FILENAME " line "   count+1 " Incorrect number of fields"; exit 1}' $file
 	if [[ $? -ne 0 ]]
 	then
 		exit 1
