88a04502f4006f722fc756340b447696b00d9012
Luis Fernando Gil Arroyo <luis.gil.arroyo@alumnos.upm.es>
2018-03-15 21:15:39 +0100

    Update testCSV.sh

--- testCSV.sh
+++ testCSV.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Name: testCSV.sh
+#Description: Validates csv files against a determined number of fields
+#Parameters: pairs of file_name number_of_fields
+#Output: Descriptions of lines with incorrect number of fields, or none if the files are correct
+#Exit Value: Number of errors found, 0 if the files were correct
+
 if [[ $(( $# % 2 )) -ne 0 ]]
 then
 	echo "Incorrect Usage: " $0 "[file  numberOfFields]"
@@ -7,6 +13,7 @@ then
 fi
 
 arguments=( "$@" )
+errors=0
 
 while [[ ${#arguments[@]} > 0 ]]
 do
@@ -15,12 +22,9 @@ do
 	awk -v n=$numberfields 'BEGIN{FS=OFS=","} NF==n{count++} NF!=n{print "ERROR in file " FILENAME " line "   count+1 " Incorrect number of fields"; errors++; count++} END {if ( errors != 0  ) {exit 1}}' $file
 	if [[ $? -ne 0 ]]
 	then
-		error="true"
+		errors=$((errors+1))
 	fi
 	arguments=("${arguments[@]:2}")
 done
 
-if [ error == "true" ]
-then
-	exit 1
-fi
+exit $errors
