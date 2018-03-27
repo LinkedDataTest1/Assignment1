8dba6f334af26bcd3694310d50bfc5cfc17ac82f
Luis Fernando Gil Arroyo <luis.gil.arroyo@alumnos.upm.es>
2018-03-27 21:05:17 +0200

    [ci skip]

--- build/testCSV.sh
+++ build/testCSV.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
 #Name: testCSV.sh
-#Description: Validates csv files against a determined number of fields
-#Parameters: pairs of file_name number_of_fields
+#Description: Validates student csv file
+#Parameters: none
 #Output: Descriptions of lines with incorrect number of fields, or none if the files are correct
-#Exit Value: Number of errors found, 0 if the files were correct
+#Exit Value: Number of errors found, 0 if the file was correct
 
-if [[ $(( $# % 2 )) -ne 0 ]]
-then
-	echo "Incorrect Usage: " $0 "[file  numberOfFields]"
-	exit 1
-fi
-
-arguments=( "$@" )
 errors=0
 
-while [[ ${#arguments[@]} > 0 ]]
-do
-	file=${arguments[0]}
-	numberfields=${arguments[1]}
+TOKEN1=8c073f6335e29d1
+TOKEN2=e06e0dbade3a8a78405449b5d
+username=$(curl -H "Authorization: token $TOKEN1$TOKEN2" -X GET "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls/${TRAVIS_PULL_REQUEST}" | jq -r '.user.login')
+
+if [ ! -f "$username-$number.csv" ]; then
+  echo "File missing. Make sure it has the correct format" "$username-$number.csv"
+  errors=$((errors+1))
+else
+	file=$username-$number.csv
+	numberfields=2
 	awk -v n=$numberfields 'BEGIN{FS=OFS=","} NF==n{count++} NF!=n{print "ERROR in file " FILENAME " line "   count+1 " Incorrect number of fields"; errors++; count++} END {if ( errors != 0  ) {exit 1}}' $file
 	if [[ $? -ne 0 ]]
 	then
 		errors=$((errors+1))
 	fi
-	arguments=("${arguments[@]:2}")
-done
+fi
 
 exit $errors
